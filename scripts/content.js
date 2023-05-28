df294d7f90abf832bb8c7fcc09f01576faa96f98
Fernando-Fernandez <fmendes@hotmail.com>
2023-05-28 14:32:27 -0500

    Improved tooltip for subflows and actions

--- scripts/content.js
+++ scripts/content.js
@@ -56,7 +56,7 @@ function setFlowDefinitionFromToolingAPI( baseUrl, sessionId ) {
 let getNodesTimeout;
 function addHoverEvents() {
     // attempt to get list of flow nodes repeatedly, in both auto-layout and free-form modes
-    let flowShapes = document.querySelectorAll( "div.node-container, span.text-element-label" );
+    let flowShapes = document.querySelectorAll( "div.node-container, span.text-element-label,div.start-node-box" );
     if( flowShapes.length <= 0 ) {
         // nodes not created, try again in 2 secs
         getNodesTimeout = setTimeout( () => {
@@ -103,6 +103,33 @@ function getNode( elementName, array ) {
     return array.find( aNode => aNode.label === elementName );
 }
 
+function appendNodeAndLine( aNode ) {
+    tooltip.appendChild( aNode );
+    tooltip.appendChild( document.createElement( "br" ) );
+}
+
+function getValue( aValue ) {
+    return aValue.elementReference ?? aValue.stringValue ?? aValue.numberValue
+                ?? aValue.booleanValue?? aValue.dateTimeValue?? aValue.dateValue;
+}
+
+function createTooltip( elementName, currentTarget, offsetHorizontal, offsetVertical ) {
+    // read flow element position and add offset
+    let leftPos = offsetHorizontal + parseInt( currentTarget.style.left );
+    leftPos = isNaN( leftPos ) ? offsetHorizontal : leftPos;
+    let topPos = offsetVertical + parseInt( currentTarget.style.top );
+    topPos = isNaN( topPos ) ? offsetVertical : topPos;
+    tooltip = document.createElement( "div" );
+    tooltip.setAttribute( "style", "border: solid 1px darkgray; background-color: lightyellow; width:30em; position: absolute; z-index: 999;"
+                                    + " top: " + topPos + "px; left: " + leftPos + "px;" );
+    let titleNode = document.createTextNode( elementName );
+    let boldNode = document.createElement( "strong" );
+    boldNode.appendChild( titleNode );
+    appendNodeAndLine( boldNode );
+
+    currentTarget.parentNode.appendChild( tooltip );
+}
+
 let tooltip;
 function displayTooltip( event, displayFlag ) {
     if( ! flowDefinition ) {
@@ -115,6 +142,12 @@ function displayTooltip( event, displayFlag ) {
 
     // if flag = false, keep it without tooltip
     if( ! displayFlag ) {
+        return;
+    }
+
+    // tooltip on the start flow element
+    if( event.currentTarget.className === 'start-node-box' ) {
+        createTooltip( 'START FLOW', event.currentTarget, 280, 25 );
         return;
     }
 
@@ -136,65 +169,76 @@ function displayTooltip( event, displayFlag ) {
         return;
     }
 
-    // read flow element position and add offset
-    let leftPos = 150 + parseInt( event.currentTarget.style.left );
-    leftPos = isNaN( leftPos ) ? 150 : leftPos;
-    let topPos = 25 + parseInt( event.currentTarget.style.top );
-    topPos = isNaN( topPos ) ? 25 : topPos;
+    createTooltip( elementName, event.currentTarget, 150, 40 );
 
-    // create new tooltip with flow element name at the top
-    tooltip = document.createElement( "div" );
-    tooltip.setAttribute( "style", "border: solid 1px darkgray; background-color: lightyellow; width:30em; position: absolute; z-index: 999;"
-                                    + " top: " + topPos + "px; left: " + leftPos + "px;" );
-    let titleNode = document.createTextNode( elementName );
-    let boldNode = document.createElement( "strong" );
-    boldNode.appendChild( titleNode );
-    tooltip.appendChild( boldNode );
-    tooltip.appendChild( document.createElement( "br" ) );
+    // get subflow name if calling subflow
+    try {
+        let subflowName = node.flowName;
+        if( subflowName ) {
+            let subflowNode = document.createTextNode( `(${subflowName})` );
+            appendNodeAndLine( subflowNode );
+        }
+    } catch( e ) {
+    }
 
     // add field assignments if creating record
     node.inputAssignments?.forEach( aField => {
-        let assignmentNode = document.createTextNode( aField.field + ' = ' + aField.value.elementReference );
-        tooltip.appendChild( assignmentNode );
-        tooltip.appendChild( document.createElement( "br" ) );
+        let assignDescription = ( aField.field ?? aField.name ) + ' = ' + getValue( aField.value );
+        let assignmentNode = document.createTextNode( assignDescription );
+        appendNodeAndLine( assignmentNode );
     } );
     
     // add fields if screen element
     node.fields?.forEach( aField => {
         let fieldText = aField.fieldText;
+
+        if( aField.fieldType === "DisplayText" ) {
+            // remove HTML
+            fieldText = fieldText.replaceAll( /\<\/?.*?\>/g, '' );
+        }
+
+        if( aField.fieldType === "ComponentInstance" ) {
+            fieldText = aField.inputParameters?.reduce( ( accumulator, currentValue ) => 
+                                                    accumulator + getValue( currentValue.value ) + ", "
+                                                    , "" );
+        }
+
         if( ! fieldText ) {
             fieldText = aField.outputParameters?.reduce( ( accumulator, currentValue ) => 
-                                        accumulator + " / " + currentValue.assignToReference + " = " + currentValue.name
+                                        accumulator + " / " + getValue( currentValue ) + " = " + currentValue.name
                                         , "" );
             if( fieldText.length > 2 ) {
                 fieldText = fieldText.substring( 2 );
             }
         }
-        let fieldsNode = document.createTextNode( fieldText );
-        tooltip.appendChild( fieldsNode );
-        tooltip.appendChild( document.createElement( "br" ) );
+        let fieldsNode = document.createTextNode( `${aField.fieldType}: ${fieldText}` );
+        appendNodeAndLine( fieldsNode );
     } );
     
     // add fields if action
     if( node.actionName ) {
         let fieldsNode = document.createTextNode( node.actionName + " (" + node.actionType + ")" );
-        tooltip.appendChild( fieldsNode );
+        appendNodeAndLine( fieldsNode );
+        
+        node.inputParameters?.forEach( aField => {
+            let paramDescription = ( aField.field ?? aField.name ) + ' = ' + getValue( aField.value );
+            let paramNode = document.createTextNode( paramDescription );
+            appendNodeAndLine( paramNode );
+        } );
+        
     };
 
     // add rules if decision
     node.rules?.forEach( anItem => {
         let ruleLabelNode = document.createTextNode( anItem.label );
-        tooltip.appendChild( ruleLabelNode );
-        tooltip.appendChild( document.createElement( "br" ) );
+        appendNodeAndLine( ruleLabelNode );
 
         anItem.conditions?.forEach( condition => {
             let fieldsNode = document.createTextNode( condition.leftValueReference 
                 + " " + ( condition.operator == "EqualTo" ? "=" : condition.operator ) + " "
-                + ( condition.rightValue?.stringValue ?? condition.rightValue?.numberValue ?? condition.rightValue?.dateValue
-                ?? condition.rightValue?.booleanValue ?? condition.rightValue?.dateTimeValue ?? condition.rightValue?.elementReference ) );
+                + getValue( condition.rightValue ) );
         
-            tooltip.appendChild( fieldsNode );
-            tooltip.appendChild( document.createElement( "br" ) );
+            appendNodeAndLine( fieldsNode );
         } );
     } );
 
@@ -202,10 +246,8 @@ function displayTooltip( event, displayFlag ) {
     node.filters?.forEach( anItem => {
         let fieldsNode = document.createTextNode( anItem.field 
                                                 + " " + ( anItem.operator == "EqualTo" ? "=" : anItem.operator ) + " "
-                                                + ( anItem.value?.stringValue ?? anItem.value?.numberValue ?? anItem.value?.dateValue
-                                                ?? anItem.value?.booleanValue ?? anItem.value?.dateTimeValue ?? anItem.value?.elementReference ) );
-        tooltip.appendChild( fieldsNode );
-        tooltip.appendChild( document.createElement( "br" ) );
+                                                + getValue( anItem.value ) );
+        appendNodeAndLine( fieldsNode );
     } );
 
     // add fields if assignment
@@ -213,10 +255,13 @@ function displayTooltip( event, displayFlag ) {
         let fieldsNode = document.createTextNode( anItem.assignToReference 
                                                 + " " + ( anItem.operator == "Assign" ? "=" : "" )
                                                 + ( anItem.operator == "Add" ? "appended with" : "" ) + " "
-                                                + ( anItem.value?.stringValue ?? anItem.value?.numberValue ?? anItem.value?.dateValue
-                                                ?? anItem.value?.booleanValue ?? anItem.value?.dateTimeValue ?? anItem.value?.elementReference ) );
-        tooltip.appendChild( fieldsNode );
-        tooltip.appendChild( document.createElement( "br" ) );
+                                                + getValue( anItem.value ) );
+        appendNodeAndLine( fieldsNode );
+    } );
+
+    // add parameters if subflow
+    node.subflows?.forEach( anItem => { 
+        console.log( anItem );
     } );
     
     // add tooltip to the parent of the current flow element
