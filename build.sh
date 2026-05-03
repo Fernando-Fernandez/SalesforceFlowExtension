d08d332bab2a86d19a1a845478aa9359fd7c5e55
Fernando-Fernandez <fmendes@hotmail.com>
2026-05-03 01:03:59 -0500

    Changed build to CRX instead of zip

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-zip -r SalesforceFlowExtension.zip manifest.json LICENSE README.MD popup.* scripts/ images/
+#zip -r SalesforceFlowExtension.zip manifest.json LICENSE README.MD popup.* scripts/ images/
+
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --pack-extension=/Users/fmendes/Projects/SalesforceFlowExtension --pack-extension-key=/Users/fmendes/Projects/SalesforceFlowExtensionCert/privateKey.pem
